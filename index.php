3370ba6d26c1f7245ce310385525f2af41ad81f6
Luiz Henrique Brito Mongelos <lhmongelos@gmail.com>
2020-05-16 22:34:56 -0400

    Teste de commit com PHP

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    $name = "Luiz Henrique Brito Mongelos.";
+    $vaga = "Front-End Developer.";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    
+    <title>Potifólio</title>
+</head>
+<body>
+    <header>
+        <nav class="menu">
+            <ul>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Portfólio</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+
+        <h1 class="site-title"><?php echo $name?></h1>
+        <h2 class="site-subtitle"><?php echo $vaga?></h2>
+
+    </header>
+</body>
+</html>
